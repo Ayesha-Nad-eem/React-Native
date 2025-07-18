a57a13075b1ff2cec4adc569e0be1deea0529ef5
Ayesha-Nad-eem <an0124163@gmail.com>
2025-07-18 05:39:34 -0700

    usecolorScheme hook, useWindowDimensions hook

--- LittleLemon/app/LittleLemonHeader.tsx
+++ LittleLemon/app/LittleLemonHeader.tsx
@@ -3,7 +3,7 @@ import { View, Text } from "react-native";
 export default function LittleLemonHeader() {
     return (
         <View className="lemon-header">
-               <Text className="lemon-header-text font-bold"> Little Lemon</Text> {' '}
+               <Text className="lemon-header-text font-bold"> Little Lemon</Text>
         </View>
     )
 }
--- LittleLemon/app/Welcome.tsx
+++ LittleLemon/app/Welcome.tsx
@@ -1,11 +1,14 @@
 import * as React from 'react';
-import { View, Text, ScrollView, Image } from 'react-native';
+import { View, Text, ScrollView, Image, useColorScheme,useWindowDimensions } from 'react-native';
 
 export default function Welcome() {
+  const colorScheme = useColorScheme();
+  const windowDimensions = useWindowDimensions();
   return (
     <ScrollView
       indicatorStyle="white"
       className="flex-1 p-5"
+      style={{ backgroundColor: colorScheme === 'dark' ? '#333' : '#9c9c9c' }}
     >
       <View className='flex-row justify-center m-[10px]'>
         <Image
@@ -20,6 +23,9 @@ export default function Welcome() {
         </Text>
       </View>
       <Text className="welcome-para">Little Lemon is a charming neighborhood bistro that serves simple food and classic cocktails in a lively but casual environment. We would love to hear more about your experience with us!</Text>
+      {/* <Text>{colorScheme}</Text> */}
+      {/* <Text className="welcome-para">The current window dimensions are {windowDimensions.width} x {windowDimensions.height} </Text>
+      <Text className="welcome-para">Font scale: {windowDimensions.fontScale}</Text> */}
     </ScrollView>
   )
 }
